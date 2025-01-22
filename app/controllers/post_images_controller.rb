99c05a4821f8db0e066c7455bac2197c9af9defd
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2025-01-22 23:55:24 +0000

    2-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,6 +8,10 @@ class PostImagesController < ApplicationController
     @post_images = PostImage.all
   end
 
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
